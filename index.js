fe46898a664ace0868a045ffe031f7be4e3e9b12
madeadhi <madeadhi228@gmail.com>
2019-10-10 05:35:38 +0800

    Test SELECT

--- index.js
+++ index.js
@@ -1,24 +1,56 @@
 require("dotenv").config();
 const express = require("express");
+const Sequelize = require("sequelize");
+const { WebhookClient, Card } = require("dialogflow-fulfillment");
 const app = express();
 const port = process.env.PORT;
-const { WebhookClient } = require("dialogflow-fulfillment")
+
+const sequelize = new Sequelize("db_restoran", "madeadhi", "madeadhi12", {
+  host: "db4free.net",
+  dialect: "mysql"
+});
 
 app.use(express.json());
 
 app.post("/", (request, response, next) => {
-    const agent = new WebhookClient({ request, response });
-    let intent = new Map();
+  const agent = new WebhookClient({ request, response });
+  let intent = new Map();
 
-    const booking = (agent) => {
-        agent.add(`Booking memek?`)
-    }
+  const booking = agent => {
+    agent.add(`Booking memek?`);
+  };
 
-    intent.set('booking', booking);
+  const pesan = agent => {
+    sequelize
+      .query("SELECT * FROM tb_menu")
+      .then(([result]) => {
+        agent.add(
+          "Berikut merupakan daftar makanan dan minuman di Warung Robert:"
+        );
+        result.map(data =>
+          agent.add(
+            new Card({
+              title: data.nama_makanan,
+              imageUrl: data.gambar,
+              text: `${data.keterangan}\nHarga: ${Number(
+                data.harga
+              ).toLocaleString("ID", { style: "currency", currency: "IDR" })}`,
+              buttonText: "Pesan"
+            })
+          )
+        );
+      })
+      .catch(() =>
+        agent.add("Mohon maaf, terjadi kesalahan. Silahkan ulangi kembali")
+      );
+  };
 
-    agent.handleRequest(intent)
-})
+  intent.set("booking", booking);
+  intent.set("pesan_mknmin", pesan);
 
-app.listen(port, ()=> {
-    console.log(`server start at ${port}`);
+  agent.handleRequest(intent);
+});
+
+app.listen(port, () => {
+  console.log(`server start at ${port}`);
 });
