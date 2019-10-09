a5f6ecc6311e1df9a5b4cea80d21482af591f3de
madeadhi <madeadhi228@gmail.com>
2019-10-09 20:18:45 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT;
 const { WebHookClient } = require("dialogflow-fulfillment")
-
-mongoose.connect(mongodb_uri, { useNewUrlParser: true})
 
 app.use(express.json());
 
